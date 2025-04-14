be67ef5813c4f9c6e64d5e35946c1028ec346ff7
Gaurav Shimpi <73295020+Gaurav270901@users.noreply.github.com>
2025-04-14 22:52:47 +0530

    Time: 0 ms (100%), Space: 11.2 MB (46.06%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-      
+      //here we have to check how many times did array changes its inversion , if its sorted only then it will  change inversion zero times if its sorted and rotated then one time and more than that is not sorted and rotated 
        int countInversion = 0 ;
        int length = nums.size(); 
        for(int i = 0 ; i < length;i++) 
