836c356d729943796bd0c0e6d4aa2f01c3443bcb
Gaurav Shimpi <73295020+Gaurav270901@users.noreply.github.com>
2025-04-17 00:03:46 +0530

    Time: 0 ms (100%), Space: 20.5 MB (98.06%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int temp = 0; 
-        for(int i = 0 ; i < nums.size();i++){
-            temp = temp ^ nums[i];
+        for(int num : nums ){
+            temp = temp ^ num;
         }
         return temp ; 
     }
