59acc4e144b4d6c83cc0f2af03c6cd68a12c6ff6
Gaurav Shimpi <73295020+Gaurav270901@users.noreply.github.com>
2025-04-23 20:29:02 +0530

    Time: 49 ms (61.16%), Space: 29 MB (89.31%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& num) {
-        set<vector<int> >rs;
-        sort(num.begin(), num.end());
-        int i, j, k;
-        int n = num.size();
-        for (i = 0; i < n - 2; i++) {
-            j = i+1;
-            k = n-1;
-            while (j < k) {
-                int sum = num[i] + num[j] + num[k];
-                if (sum == 0) {
-                    vector<int> tmp(3);
-                    tmp[0] = num[i];
-                    tmp[1] = num[j];
-                    tmp[2] = num[k];
-                    rs.insert(tmp);
-                    j++;
-                    k--;
-                } else if (sum > 0) {
-                    k--;
-                } else if (sum < 0) {
-                    j++;
+    vector<vector<int>> threeSum(vector<int>& arr) {
+        vector<vector<int>> ans ; 
+        sort(arr.begin() , arr.end());
+
+        for(int i = 0 ; i < arr.size() ; i++){
+            if(i > 0 && arr[i]==arr[i-1]) continue ; 
+
+            int left = i+1 ; 
+            int right = arr.size()-1;
+
+            while(left < right){
+                int sum = arr[i]+arr[left]+arr[right] ; 
+                if(sum==0){
+                    ans.push_back({arr[i],arr[left],arr[right]});
+                
+                    while(left < right && arr[left]==arr[left+1]) left++;
+                    while(left < right && arr[right]==arr[right-1]) right--;
+
+                    left++;
+                    right--;
                 }
+                else if(sum < 0) left++ ;
+                else right --;
             }
         }
-        vector<vector <int> >result (rs.begin(), rs.end());
-        return result;
+        return ans ; 
     }
 };
