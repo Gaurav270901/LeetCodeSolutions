e53ea05781ecf7443a8472a44d874947e01b10e1
Gaurav Shimpi <73295020+Gaurav270901@users.noreply.github.com>
2025-04-23 20:36:39 +0530

    Time: 43 ms (87.13%), Space: 29.1 MB (51.94%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,6 +1,34 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& arr) {
+    // vector<vector<int>> threeSum(vector<int>& arr) {
+    //     vector<vector<int>> ans ; 
+    //     sort(arr.begin() , arr.end());
+
+    //     for(int i = 0 ; i < arr.size() ; i++){
+    //         if(i > 0 && arr[i]==arr[i-1]) continue ; 
+
+    //         int left = i+1 ; 
+    //         int right = arr.size()-1;
+
+    //         while(left < right){
+    //             int sum = arr[i]+arr[left]+arr[right] ; 
+    //             if(sum==0){
+    //                 ans.push_back({arr[i],arr[left],arr[right]});
+                
+    //                 while(left < right && arr[left]==arr[left+1]) left++;
+    //                 while(left < right && arr[right]==arr[right-1]) right--;
+
+    //                 left++;
+    //                 right--;
+    //             }
+    //             else if(sum < 0) left++ ;
+    //             else right --;
+    //         }
+    //     }
+    //     return ans ; 
+    // }
+
+     vector<vector<int>> threeSum(vector<int>& arr) {
         vector<vector<int>> ans ; 
         sort(arr.begin() , arr.end());
 
@@ -12,17 +40,15 @@ public:
 
             while(left < right){
                 int sum = arr[i]+arr[left]+arr[right] ; 
-                if(sum==0){
+                if(sum > 0) right-- ; 
+                else if (sum < 0 ) left++;
+                else {
                     ans.push_back({arr[i],arr[left],arr[right]});
-                
-                    while(left < right && arr[left]==arr[left+1]) left++;
-                    while(left < right && arr[right]==arr[right-1]) right--;
-
                     left++;
-                    right--;
+
+                    while(left < right && arr[left]==arr[left-1]) left++;
                 }
-                else if(sum < 0) left++ ;
-                else right --;
+            
             }
         }
         return ans ; 
