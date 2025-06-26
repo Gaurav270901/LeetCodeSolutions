b0ee3313f5823c82f38c72bdfd5238314a53d3d1
Gaurav Shimpi <73295020+Gaurav270901@users.noreply.github.com>
2025-06-26 21:23:38 +0530

    Time: 7 ms (68.71%), Space: 13.8 MB (8.42%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,18 +9,32 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        ListNode *slow = head , *fast = head;
-        while(fast != nullptr && fast->next != nullptr){
-            fast = fast->next->next ;
-            slow = slow->next;
-            if(fast == slow) {
-            slow = head ; 
-                while(slow != fast){
-                    slow = slow->next ;
-                    fast = fast->next ;
-                }
-                return slow ;
+        // ListNode *slow = head , *fast = head;
+        // while(fast != nullptr && fast->next != nullptr){
+        //     fast = fast->next->next ;
+        //     slow = slow->next;
+        //     if(fast == slow) {
+        //     slow = head ; 
+        //         while(slow != fast){
+        //             slow = slow->next ;
+        //             fast = fast->next ;
+        //         }
+        //         return slow ;
+        //     }
+        // }
+        // return nullptr;
+
+        //solving it my hashmap
+
+        unordered_map<ListNode* , int> mp ;
+
+        ListNode *ptr = head ;
+        while(ptr!=nullptr){
+            if(mp.count(ptr)!=0) return ptr ;
+            else {
+                mp[ptr] = 1;
             }
+            ptr = ptr->next;
         }
         return nullptr;
     }
