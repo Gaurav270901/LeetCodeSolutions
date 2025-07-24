13b691ed81a8537980afa98e69c2fd71a9cfe51f
Gaurav Shimpi <73295020+Gaurav270901@users.noreply.github.com>
2025-07-24 20:34:55 +0530

    Time: 3 ms (56.02%), Space: 14.1 MB (54.46%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public: 
 
-    void backtrack(vector<int>& candidates , vector<int> &current , int currentSum , vector<vector<int>>& ans , int target , int index , int size){
+    void backtrack(vector<int>& candidates , vector<int> &current , int currentSum , vector<vector<int>>& ans , int target , int index ){
        //
             if(currentSum == target){
                 ans.push_back(current);
                 return ;
             }
             
-        if(currentSum > target || index == size) return ;
+        if(currentSum > target || index == candidates.size()) return ;
 //first we will include current element in our addition and then take that element again in repeat call as seen in backtrack we are sentting the same index
         current.push_back(candidates[index]);
-        backtrack(candidates,current,currentSum + candidates[index],ans , target,index , size);
+        backtrack(candidates,current,currentSum + candidates[index],ans , target,index );
 
 //now we will exclude that element from combination and take next index
         current.pop_back();
-        backtrack(candidates , current , currentSum , ans , target , index+1 , size);
+        backtrack(candidates , current , currentSum , ans , target , index+1);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans ;
         vector<int> current ;
 
-        backtrack(candidates , current , 0 , ans , target ,0 ,candidates.size() );
+        backtrack(candidates , current , 0 , ans , target ,0 );
         return ans ;
     }
 };
