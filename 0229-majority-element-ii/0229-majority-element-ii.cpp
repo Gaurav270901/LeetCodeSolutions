d97f1593eb6f7f6dfd843fb449676fdf013eb911
Gaurav Shimpi <73295020+Gaurav270901@users.noreply.github.com>
2026-01-26 21:03:29 +0530

    Time: 0 ms (100%), Space: 23.6 MB (82.49%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,47 +1,34 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        //we will take two pointer and two counter 
-        //intuation here is there can only maximum two elements exists in array with frequency grerater than n/3
-        //then we will follow Moore's voting algorithm
-        // Step 1: There can be at most two elements in the array that appear more than n/3 times.
-        // So, we track two potential candidates (elm1, elm2) and their counts (cnt1, cnt2).
-        
-        vector<int> ans ; 
-        int cnt1 = 0 , cnt2 = 0 , elm1 = INT_MIN , elm2 = INT_MIN , n = nums.size() ; 
-
-        // Step 2: First pass — Find potential candidates using Moore's Voting.
-        //If we see the same element again, we increment its counter.
-        //If we see a new element and a counter is 0, we replace the candidate.
-        //If both counters are non-zero and the number is different, we reduce both counters.
-        for(int i = 0 ; i < n ; i++){
-            if(nums[i]==elm1) cnt1++ ; 
-            else if(nums[i]==elm2) cnt2++;
-            else if(cnt1 == 0 ){
-                elm1 = nums[i];
-                cnt1 = 1;
-            }
-            else if(cnt2 == 0){
-                elm2=nums[i];
-                cnt2 = 1 ;
-            }
-            else{
-                cnt1--;
-                cnt2--;
-            }
+      int cdOneCount = 0 , cdTwoCount = 0 , cdOne = INT_MIN , cdTwo = INT_MIN ;
+      vector<int> ans ; 
+      for(int i = 0 ; i < nums.size();i++){
+        if(nums[i]==cdOne) cdOneCount++;
+        else if (nums[i]==cdTwo) cdTwoCount++;
+        else if (cdOneCount == 0){
+            cdOne = nums[i];
+            cdOneCount = 1 ;
         }
-
-        // Step 3: Second pass — Verify if the candidates appear more than n/3 times.
-        cnt1 = cnt2 = 0 ;
-        for(int i = 0 ; i < n ; i++){
-            if(nums[i] == elm1) cnt1++;
-            if(nums[i] == elm2) cnt2++;
+        else if(cdTwoCount == 0){
+            cdTwo = nums[i];
+            cdTwoCount = 1;
         }
+        else{
+            cdOneCount--;
+            cdTwoCount--;
+        }
+      }
+    
+        cdOneCount = cdTwoCount = 0 ;
+      for(int i = 0 ; i < nums.size();i++){
+        if(nums[i]==cdOne) cdOneCount++;
+        if(nums[i]==cdTwo) cdTwoCount++;
+      }
 
-        if(cnt1 > n/3) ans.push_back(elm1);
-        if(cnt2 > n/3) ans.push_back(elm2);
-
-
-        return ans ; 
+        if ((cdOneCount ) > nums.size()/3 ) ans.push_back(cdOne);
+        if ((cdTwoCount ) > nums.size()/3 ) ans.push_back(cdTwo);
+        return ans;
     }
+
 };
