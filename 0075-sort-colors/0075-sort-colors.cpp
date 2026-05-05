0fe369ba3b22296974a1587a7f8f58b0a58a0671
Gaurav Shimpi <73295020+Gaurav270901@users.noreply.github.com>
2026-05-06 00:53:43 +0530

    Time: 0 ms (100%), Space: 11.7 MB (10.73%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     void sortColors(vector<int>& arr) {
-        if(arr.size() == 1) return ; 
-        int low = 0 , high = arr.size()-1,mid=0 ;
-        while(mid<=high)
-        {
-            if(arr[mid]==0){
-                swap(arr[mid],arr[low]);
-                low++;
-                mid++;//as mid will be replaced by the number which is already process we will increatement that pointer to the next index 
+        int left = 0 , right = arr.size()-1 , mid = 0 ;
+        while(mid <= right){
+            if(arr[mid] == 0){
+                swap(arr[mid] , arr[left]);
+                left++;
+                mid++;
             }
-        
-            else if(arr[mid]==2){
-                swap(arr[mid] , arr[high]);
-                high--;
+            else if(arr[mid] == 2){
+                swap(arr[mid] ,arr[right]);
+                right--;
             }
-            else mid++;
+            else{
+                mid++;
+            }
         }
     }
 };
