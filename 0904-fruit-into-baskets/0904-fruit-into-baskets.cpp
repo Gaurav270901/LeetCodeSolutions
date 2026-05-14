ee8d4f1517de9fccf3c188eb30078da4c1ff085b
Gaurav Shimpi <73295020+Gaurav270901@users.noreply.github.com>
2026-05-14 18:03:12 +0530

    Time: 51 ms (28.22%), Space: 85 MB (34.39%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -4,6 +4,7 @@ public:
         
         unordered_map<int,int> map ; 
         int high = 0 , low = 0 , n = fruits.size() , ans = INT_MIN;
+        if(n == 1) return n ;
         while(high < n){
             map[fruits[high]]++;
 
