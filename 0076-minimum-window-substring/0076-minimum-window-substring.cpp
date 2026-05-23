8e52292f240b9a156e619ed789831643be235f6f
Gaurav Shimpi <73295020+Gaurav270901@users.noreply.github.com>
2026-05-23 09:47:01 +0530

    Time: 598 ms (5.01%), Space: 379.9 MB (5.13%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -2,43 +2,33 @@ class Solution {
 public:
     string minWindow(string s, string t) {
 
-        vector<int> freq(256,0);
-        int high = 0 , low = 0 , result = INT_MAX , n = s.size() , required = t.size() , start , end ; 
-
+        vector<int> tfreq(256,0)  , sfreq(256,0);
+        int high = 0 , low = 0 , result = INT_MAX , n = s.size() , start ; 
         for(int i = 0 ; i < t.size() ; i++){
-            freq[t[i]]++;
+            tfreq[t[i]]++;
         }
         while(high < n ){
-           //check if character present in map
-           if(freq[s[high]]>0){
-            required--;//we got one required character so reduce the number
-           }
-           //add first value in result , but here we will decrease the freq of map for each character
-           freq[s[high]]--;
-
-           //while we get correct ans try to minimize the window by moving low and maintaing required count
-           while(required == 0){
-            int len = high - low + 1 ; 
-            if(len < result){
-                start = low ; 
-                result = len ;
+            sfreq[s[high]]++;
+            while(allCharPresent(sfreq , tfreq )){
+                int len = high-low+1;
+                if(len < result){
+                   start = low ;
+                   result = len ;
+                }
+                sfreq[s[low]]--;
+                low++;
             }
-            freq[s[low]]++;
-            if(freq[s[low]] > 0){
-                required++;
-            }
-            low++;
-           }
-           high++;
+            high++;
+           
         }
 
         return (result < INT_MAX) ? s.substr(start , result) : "";
     }
 
-    bool allCharPresent(vector<int> sfreq , vector<int> tfreq , string t){
-        for(int i = 0 ; i < t.size() ; i++){
-            if(sfreq[t[i]]<tfreq[t[i]])
-                return false ;
+    bool allCharPresent(vector<int> sfreq , vector<int> tfreq ){
+        for(int i = 0 ; i < 256 ; i++){
+            if(sfreq[i] < tfreq[i]  )
+                return false;
         }
         return true ;
     }
