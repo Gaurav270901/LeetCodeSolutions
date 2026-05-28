a49d69e3641cc662d99d54bac7737d2fc7c42f02
Gaurav Shimpi <73295020+Gaurav270901@users.noreply.github.com>
2026-05-28 12:12:33 +0530

    Time: 3 ms (67.63%), Space: 65.1 MB (40.77%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
+        //use two pointer approact as all elements of array are less than n 
+        // and we can say that [1,3,4,2,2] for this 
+        // 0 index -> 1 , 1 index -> 3 , 3 index -> 2
+        // in short its working like linked list with next node of i is stored at nums[i]  0->1->3->2->4->2 
+        //we need to apply the same logic as to find starting point of cycle in LL
         int slow = 0 , fast = 0 ;
         while(true){
             slow = nums[slow];
