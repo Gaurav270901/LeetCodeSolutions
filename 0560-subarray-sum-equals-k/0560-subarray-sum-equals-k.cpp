ee2613c365e69218aaf3f3a9274358f938a32a87
Gaurav Shimpi <73295020+Gaurav270901@users.noreply.github.com>
2026-06-17 12:59:54 +0530

    Time: 79 ms (25.32%), Space: 53.8 MB (6.78%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -4,10 +4,10 @@ public:
         map<int,int> mp ;
         mp[0] = 1 ; 
         int sum = 0 , ans = 0 ;
-        
+
         for(int i = 0 ; i < nums.size() ; i++){
             sum+=nums[i];   
-            if(mp.find(sum-k) != mp.end()){
+            if(mp[sum-k] != 0){
                 ans += mp[sum - k];
             }
 
