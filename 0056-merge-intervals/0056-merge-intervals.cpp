b62b51a0fdf8716cde945c78d67ddb56e6277cb0
Gaurav Shimpi <73295020+Gaurav270901@users.noreply.github.com>
2026-06-28 21:40:10 +0530

    Time: 4 ms (65.75%), Space: 23.9 MB (31.09%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        //first we will sort interval array and then after that we will compare it with adjacent element
-        //we will add given interval directly when they are not overlapption and condition for that:
-        //condition 1 : if ans array is empty 
-        //condition 2 : if start element of current interval is greater that the end element of previous interval
-
-        //if we dont get overlapping interval then we will merge them , as interval array is sorted it is clear that and.back()[0]  < interval[i][0] every time 
-        //so for overlapping interval we need to only take care of last index , we will take the last index as maximum from ans.back()[1] , interval[i][1]
-        sort(intervals.begin() , intervals.end());
-        vector<vector<int>> ans ; 
-
-        for(int i = 0 ; i < intervals.size() ; i++){
-            if(ans.size()==0 || intervals[i][0] > ans.back()[1])
-                ans.push_back(intervals[i]);
-            else {
-                //we will update ans array as per maximum from ans and current interval last element
-                ans.back()[1] = max(ans.back()[1] , intervals[i][1]);
-            }
-            
-        }
-        return ans ;
+  static bool sortByStart(vector<int> &first , vector<int> &second){
+        return first[0] < second[0] ;
     }
+
+    vector<vector<int>> merge(vector<vector<int>>& intr) {
+        vector<vector<int>> result ; 
+        sort(intr.begin() , intr.end() , sortByStart);
+        int start1 = intr[0][0] , end1 = intr[0][1];
+        for(int i = 1 ; i < intr.size() ; i++){
+            int start2 = intr[i][0];
+            int end2 = intr[i][1];
+
+            if(start2 <= end1){
+                end1 = max(end1 , end2);
+                continue;
+            }
+
+            result.push_back({start1 , end1});
+            start1 = start2 ; 
+            end1 = end2 ;
+        }
+        result.push_back({start1 , end1});
+        return result;
+    }
+  
 };
