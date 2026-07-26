52d13dcdc3515472367c7209448cbf21c4806438
Gaurav Shimpi <73295020+Gaurav270901@users.noreply.github.com>
2026-07-26 15:16:01 +0530

    Time: 0 ms (100%), Space: 11.3 MB (40.15%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -10,20 +10,24 @@
  */
  //traverse to the left node when you are on left node then start reversing the list for right - left + 1 time , store left-1 node  and right +1 node to for attaching it to the liked list later on
  //as we are need to keep track of previous node to the left , but in case where left = 1 , i.e head, we dont have any previous node so we will add one dummy node in our list that will help us to handle such cases where left = 1 
+
+ //other approach 
+ //keep travesing while you find left node and also keep track of prev node , once you on left node start reversing from there 
+ //after reversing you need to connect reveredHead to the prevleft node , if prevleft is present then connect prevhead to the reversedhead , and return head
+ //if not present that means left == 1 at that time return prev
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
         if(!head || left == right) return head ;
 
-        ListNode dummy(0);
-        dummy.next = head ;
-        ListNode *prevLeft  = &dummy;
+        ListNode *prevLeft  = NULL;
+        ListNode *curr = head ;
 
         for(int i = 1 ; i < left ; i++){
-            prevLeft = prevLeft->next ;
+            prevLeft = curr ;
+            curr = curr->next ;
         }
      
-        ListNode *curr = prevLeft->next ;
         ListNode *lastNode = curr ;
         ListNode *prev = NULL ; 
 
@@ -37,9 +41,12 @@ public:
             curr = nextNode ;
         }
 
-        prevLeft->next  = prev ; 
         lastNode->next = curr ; 
-        return dummy.next ;
+        if(prevLeft) {
+            prevLeft->next = prev ;
+            return head ;
+        }
+        return prev;
     }
 
 };
