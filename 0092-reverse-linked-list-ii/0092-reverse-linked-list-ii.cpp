29bebcb300386681b2ca59611e4901910b54de89
Gaurav Shimpi <73295020+Gaurav270901@users.noreply.github.com>
2026-07-26 15:04:41 +0530

    Time: 0 ms (100%), Space: 11.3 MB (40.15%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -9,6 +9,7 @@
  * };
  */
  //traverse to the left node when you are on left node then start reversing the list for right - left + 1 time , store left-1 node  and right +1 node to for attaching it to the liked list later on
+ //as we are need to keep track of previous node to the left , but in case where left = 1 , i.e head, we dont have any previous node so we will add one dummy node in our list that will help us to handle such cases where left = 1 
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
