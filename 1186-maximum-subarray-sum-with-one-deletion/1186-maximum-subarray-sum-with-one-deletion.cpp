13f5e327bc904f7bc095ce5d65c2bc0bb7228765
Gaurav Shimpi <73295020+Gaurav270901@users.noreply.github.com>
2026-07-28 22:40:26 +0530

    Time: 4 ms (13.08%), Space: 27.1 MB (95.94%) - LeetHub

--- 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.cpp
+++ 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
-//prevNodelete
-//prevOneDelete
-//find max sum with no delete first , 
-//for one delete find max between adding that number in one delete or removing that number from nodelete
-    int maximumSum(vector<int>& arr) {
-        int ans = arr[0] , noDelete = arr[0] , oneDelete = INT_MIN  , n = arr.size() ;
+     int maximumSum(vector<int>& arr) {
+        int ans = arr[0] , noDelete = arr[0] , oneDelete = 0  , n = arr.size() ;
         if(n < 2) return ans ; 
         for(int i = 1 ; i < n ; i++){
             int prevNoDelete = noDelete ; 
             int prevOneDelete = oneDelete ; 
-            if(prevOneDelete == INT_MIN) prevOneDelete = 0; // for first element prevOnedelete will be 0 
             noDelete = max(noDelete+arr[i] , arr[i]);
             oneDelete = max(prevNoDelete , prevOneDelete + arr[i]);
             ans = max(ans , max(noDelete , oneDelete));
