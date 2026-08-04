aaa5fe9e716e93f3da55dd85ba1b8808718b7555
Gaurav Shimpi <73295020+Gaurav270901@users.noreply.github.com>
2026-08-05 01:09:06 +0530

    Time: 0 ms (100%), Space: 15.2 MB (36.83%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,32 +1,42 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int low = 0 , high = nums.size()-1 ; 
-        while(low <= high){
-            int mid = (low+high)/2;
-            if(nums[mid]==target) return mid ; 
-            
-            if (nums[low] <= nums[mid]) {
-                if (nums[low] <= target && target < nums[mid]) {
-                    //element exists:
-                    high = mid - 1;
-                }
-                else {
-                    //element does not exist:
-                    low = mid + 1;
-                }
+    int search(vector<int>& arr, int target) {
+        int n = arr.size();
+        int low = 0 , high = arr.size()-1 , ans = -1 ;
+        while(low <= high ){
+          int mid = (low + high)/2 ;
+          int num = arr[mid];
+
+          if(num == target) return mid ;
+
+          //mid lies in first part 
+          if(num > arr[n-1]){
+            if(num < target){
+                low = mid + 1 ; 
             }
-            else { //if right part is sorted:
-                if (nums[mid] <= target && target <= nums[high]) {
-                    //element exists:
-                    low = mid + 1;
+            else{
+                if(arr[0] <= target){
+                    high = mid - 1; 
                 }
-                else {
-                    //element does not exist:
-                    high = mid - 1;
-                }
+                else
+                    low = mid + 1 ;
             }
+          }
+          else{
+            if(num > target){
+                high = mid - 1 ;
+            }
+            else{
+                if(arr[n-1] >= target){
+                    low = mid + 1 ;
+                }
+                else
+                    high = mid - 1;
+            }
+          }          
+          
         }
-        return -1 ; 
+        return ans ;
+        
     }
 };
