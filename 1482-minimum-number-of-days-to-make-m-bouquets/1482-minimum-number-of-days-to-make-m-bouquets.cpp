86f104b5298ea97b54b2cef1c637ddb67e735556
Gaurav Shimpi <73295020+Gaurav270901@users.noreply.github.com>
2026-08-06 23:52:15 +0530

    Time: 18 ms (83.7%), Space: 70.4 MB (80.59%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,52 +1,44 @@
 class Solution {
 public:
-
-//we need to find the minimun number of days by which we can make m bouquet so we have to guess the days from lowest to highest and hence we will use binary search here
-
-
-    bool countBouquet(vector<int>& arr , int m , int k , int guess){
+    int isDayValid(vector<int>& bloomDay , int m , int k , int guess){
         int flower = 0 ; 
         int bouquet = 0 ;
-
-        for(int i = 0 ; i < arr.size() ; i++){
-            if(arr[i] <= guess){
+        for(auto day : bloomDay){
+            if(day <= guess){
                 flower++;
                 if(flower == k){
-                    bouquet++;
+                    bouquet++ ; 
                     flower = 0 ;
                 }
             }
             else
                 flower = 0 ;
         }
-
         return bouquet >= m ;
     }
     int minDays(vector<int>& bloomDay, int m, int k) {
-        int n = bloomDay.size() ; 
-        long long totalFlowers =(long long) m*k;
+        long long totalDays = (long long) m*k ;
+        int n = bloomDay.size();
 
-        if(n < totalFlowers) return -1 ;
-
-        int maxDay = 1 , minDay = INT_MAX; 
-        for(int i = 0 ; i < n ; i++){
-            maxDay = max(bloomDay[i] , maxDay);
-            minDay = min(bloomDay[i] , minDay);
+        if(totalDays > n) return -1 ;
+        int minDays = INT_MAX , maxDays = 1 ;
+        for(auto day : bloomDay){
+            minDays = min(day , minDays);
+            maxDays = max(day , maxDays);
         }
 
-        int low = minDay , high = maxDay , ans = -1 ;
-
+        int low = minDays , high = maxDays , ans = -1 ;
         while(low <= high){
             int guess = (low + high) / 2 ;
 
-            if(countBouquet(bloomDay , m , k , guess)){
-                ans = guess;
-                high = guess-1 ;
+            if(isDayValid(bloomDay , m , k , guess)){
+                ans = guess ; 
+                high = guess - 1 ;
             }
             else
                 low = guess + 1 ;
         }
-        return ans ;
+        return ans;
         
     }
 };
