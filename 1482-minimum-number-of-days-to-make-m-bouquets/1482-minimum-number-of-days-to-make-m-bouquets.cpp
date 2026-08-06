8ac5b8a1f3ee508a9ab330498d464939d83d1ec9
Gaurav Shimpi <73295020+Gaurav270901@users.noreply.github.com>
2026-08-06 23:54:23 +0530

    Time: 16 ms (90.64%), Space: 70.5 MB (80.59%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,44 +1,98 @@
 class Solution {
 public:
-    int isDayValid(vector<int>& bloomDay , int m , int k , int guess){
-        int flower = 0 ; 
-        int bouquet = 0 ;
-        for(auto day : bloomDay){
-            if(day <= guess){
+
+    // Checks whether it is possible to make at least 'm' bouquets
+    // if we wait until 'guess' number of days.
+    bool isDayValid(vector<int>& bloomDay, int m, int k, int guess) {
+
+        int flower = 0;   // Count consecutive bloomed flowers
+        int bouquet = 0;  // Number of bouquets formed
+
+        for (auto day : bloomDay) {
+
+            // Flower has bloomed by 'guess' day
+            if (day <= guess) {
                 flower++;
-                if(flower == k){
-                    bouquet++ ; 
-                    flower = 0 ;
+
+                // If we collected k consecutive flowers,
+                // we can form one bouquet
+                if (flower == k) {
+                    bouquet++;
+                    flower = 0; // Reset because flowers cannot be reused
                 }
             }
-            else
-                flower = 0 ;
+            else {
+                // Consecutive sequence breaks
+                // because this flower has not bloomed yet
+                flower = 0;
+            }
         }
-        return bouquet >= m ;
+
+        // Return true if we can make at least m bouquets
+        return bouquet >= m;
     }
+
     int minDays(vector<int>& bloomDay, int m, int k) {
-        long long totalDays = (long long) m*k ;
+
+        // Total flowers required to make m bouquets
+        long long totalFlowersNeeded = 1LL * m * k;
+
         int n = bloomDay.size();
 
-        if(totalDays > n) return -1 ;
-        int minDays = INT_MAX , maxDays = 1 ;
-        for(auto day : bloomDay){
-            minDays = min(day , minDays);
-            maxDays = max(day , maxDays);
+        // Not enough flowers available
+        if (totalFlowersNeeded > n)
+            return -1;
+
+        // Find search space for binary search
+        // Minimum possible answer = earliest bloom day
+        // Maximum possible answer = latest bloom day
+        int minDay = INT_MAX;
+        int maxDay = 1;
+
+        for (auto day : bloomDay) {
+            minDay = min(day, minDay);
+            maxDay = max(day, maxDay);
         }
 
-        int low = minDays , high = maxDays , ans = -1 ;
-        while(low <= high){
-            int guess = (low + high) / 2 ;
+        int low = minDay;
+        int high = maxDay;
+        int ans = -1;
 
-            if(isDayValid(bloomDay , m , k , guess)){
-                ans = guess ; 
-                high = guess - 1 ;
+        /*
+            Binary Search on Answer
+
+            Observation:
+            If it is possible to make m bouquets on day X,
+            then it is also possible on every day > X.
+
+            Thus answer space is monotonic:
+
+            F F F F T T T T
+                    ^
+              first valid day
+
+            We need to find the first True.
+        */
+        while (low <= high) {
+
+            int guess = low + (high - low) / 2;
+
+            // Check whether current day is sufficient
+            if (isDayValid(bloomDay, m, k, guess)) {
+
+                // Possible answer found
+                ans = guess;
+
+                // Try to find a smaller valid day
+                high = guess - 1;
             }
-            else
-                low = guess + 1 ;
+            else {
+
+                // Need more days for flowers to bloom
+                low = guess + 1;
+            }
         }
+
         return ans;
-        
     }
 };
