adaf1316586afa404a1ee81a6a5102f240298f3e
Gaurav Shimpi <73295020+Gaurav270901@users.noreply.github.com>
2026-09-04 23:53:27 +0530

    Time: 0 ms (100%), Space: 22.4 MB (77.89%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.cpp
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
-//we need to guess h here which also represent the h books for which researcher got minimum h citation 
-//so we will guess number of books and will check if that much book are available with guess number of citation 
-//typical mistake i was doing for this problem 
-//1. i tried to take low and high array index 
-//but low should be the lowest citation and high should be highest citation 
-//and we need to guess between this range if we found book equal to guess number 
-//then that can be answer and we will try to maxmimize this value 
-//if we found citations > books , means books are less then we try to lower our guess 
-//if we found citations < books means we need to raise our guess
-//one more mistake take lowest citation as 0 , if there is only one number in array and we have to guess then we need range we cant take lowest and highest by one number so use 0 to highest as range
-    int guessCount(vector<int>& citations , int guess){
-        int count = 0 ; 
-        for(auto citation : citations){
-            if(citation >= guess) count++ ; 
-        }
-        return count ;
-    }
+//optimization to previous solution 
+//as this array is sorted 
+//we dont need to iterate through array again and again to check for citation count 
+//0 1 3 5 6 
+//we guess one index suppose 1 arr[1] = 2 all element after 2 will be greater than 2 , this is obvious 
+//and hece number of papers where minimum citations were 2 = arr.size()-i=5-1 = 4 (1 3 5 6)
+//if papers > citations , increase citation 
+//other wise decrease citation
     int hIndex(vector<int>& citations) {
         int n = citations.size();
-        int low = 0 , high = citations[n-1]; 
-        int ans = 0;
+        int low = 0 ; 
+        int high = n-1 ;
         while(low <= high){
             int guess = (low + high)/2 ;
+            int citationsCount = citations[guess] ;
 
-            int hCount = guessCount(citations , guess);
-            if(hCount >= guess){
-                ans = guess ;
-                low = guess + 1 ;
-            }
-            else 
+            int paperCount = n - guess ;
+            if(citationsCount == paperCount) return paperCount ;
+            if(citationsCount > paperCount){
                 high = guess-1 ;
+            }
+            else low = guess+1 ;
         }
-        return ans ;
+        return n-low ;
+        
     }
 };
