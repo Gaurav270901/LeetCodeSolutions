fca94c3ef4ec2b7ac7d6fbdced9a67f2dcde079b
Gaurav Shimpi <73295020+Gaurav270901@users.noreply.github.com>
2026-09-04 23:01:54 +0530

    Time: 0 ms (100%), Space: 12.5 MB (79.11%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -5,11 +5,10 @@ public:
         while(low < high){
             int mid = (low + high)/2 ;
             int value = nums[mid];
-
-            if(value > nums[mid+1])
-                high = mid ;
+            if (nums[mid] > nums[mid + 1])
+                high = mid;      // descending slope, peak lies at mid or on the left
             else
-                low = mid+1 ;
+                low = mid + 1;   // ascending slope, peak lies on the right
         }
         return low ;
     }
