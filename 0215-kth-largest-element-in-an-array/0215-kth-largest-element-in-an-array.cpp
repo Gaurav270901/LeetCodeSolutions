16729b9e94dc76793c5a3af0a02161a4e283089b
Gaurav Shimpi <73295020+Gaurav270901@users.noreply.github.com>
2026-09-07 23:19:53 +0530

    Time: 20 ms (90.35%), Space: 72.7 MB (20.05%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+
+//if we use min heap in this example then we need to find which is the smallest element in the group of k element and whenever we encounter larger element in our traversal we will add that in our heap while maintaining the size of k , hence popping top element
     int findKthLargest(vector<int>& nums, int k) {
         priority_queue<int,vector<int>,greater<int>> pq ;
         int n = nums.size();
